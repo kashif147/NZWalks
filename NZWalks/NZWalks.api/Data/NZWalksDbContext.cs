5853e0c07798c9861c88537787c7e090d8a040ba
kashif147 <kashif147@gmail.com>
2022-09-29 09:50:04 -0700

    End of Section 2

---
+++ NZWalks/NZWalks.api/Data/NZWalksDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.api.Models.Domain;
+
+namespace NZWalks.api.Data
+{
+    // Make connection, Query and Persist Data
+    public class NZWalksDbContext : DbContext
+    {
+        public NZWalksDbContext(DbContextOptions<NZWalksDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulty { get; set; }
+    }
+}
+
---
+++ NZWalks/NZWalks.api/Models/Domain/Walk.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection.PortableExecutable;
+
+namespace NZWalks.api.Models.Domain
+{
+    public class Walk
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+        public double length { get; set; }
+
+        public Guid RegionId { get; set; }
+        public Guid WalkDifficultyId { get; set; }
+
+        // Navigation Properties
+
+        public Region Region { get; set; }
+
+        public WalkDifficulty WalkDifficulty { get; set; }
+
+
+    }
+}
